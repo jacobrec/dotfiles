7e18be7fb903fdc99f7ef07278e0a0ef93a0f3fd
Jacob Reckhard <jacobreckhard@gmail.com>
2019-02-07 18:09:41 -0700

    fish git config

--- fish/config.fish
+++ fish/config.fish
@@ -9,18 +9,19 @@ set gray (set_color -o black)
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_showupstream 'informative'
 set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_char_upstream_prefix ' '
 
 # Status Chars
 set __fish_git_prompt_char_dirtystate '*'
 set __fish_git_prompt_char_stagedstate '→ '
 set __fish_git_prompt_char_untrackedfiles '☡ '
 set __fish_git_prompt_char_stashstate '↩ '
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
 
 set -U fish_prompt_pwd_dir_length 0
 set -U fish_color_cwd blue
