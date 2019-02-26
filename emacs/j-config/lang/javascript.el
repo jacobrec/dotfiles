a1ded3f1cba16f36da1fd7944ad65683e0b283a8
Jacob Reckhard <jacobrec@live.com>
2019-02-26 10:25:15 -0700

    fixed eslint in prism for my setup

--- emacs/j-config/lang/javascript.el
+++ emacs/j-config/lang/javascript.el
@@ -1,11 +1,2 @@
 (use-package web-mode
   :ensure t)
-(use-package js2-mode
-  :ensure t
-  :init
-  (flycheck-select-checker 'javascript-standard)
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-  (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode)))
-
-
-
