8cb6e7df60ef4a95c5a9a0fcc335e6924680f595
Jacob Reckhard <jacobrec@live.com>
2019-02-26 11:06:49 -0700

    fixed syntax highting for await

--- emacs/j-config/lang/javascript.el
+++ emacs/j-config/lang/javascript.el
@@ -1,4 +1,18 @@
 (use-package web-mode
   :ensure t)
+
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+    (let ((web-mode-enable-part-face nil))
+      ad-do-it)
+    ad-do-it))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode. Adjust indents"
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (flycheck-add-mode 'javascript-standard 'web-mode))
+
+(add-hook 'web-mode-hook  'my-web-mode-hook)
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
-(flycheck-add-mode 'javascript-standard 'web-mode)
