1055ff70be9285c90c3967e56c1c0a25153d695e
Jacob Reckhard <jacobreckhard@gmail.com>
2019-10-08 18:35:59 -0600

    wrote projects

---
+++ bash/projects.sh
@@ -0,0 +1,19 @@
+LOC=~/.project_data
+function projects () {
+    if [ -z $1 ]; then
+        projects recent
+    elif [ $1 = "set" ]; then
+        x=$PWD
+        if [ -n "$3" ]; then
+            x=$3
+        fi
+        echo $(cat $LOC | jq ". += {\"$2\": \"$x\"}") > $LOC
+    elif [ $1 = "ls" ]; then
+        cat $LOC | jq -C | head -n -1 | tail -n +2 | sed s/\"//g | sed s/,//
+    else
+        pushd $PWD > /dev/null
+        x=$(cat $LOC | jq -r ".[\"$1\"]")
+        projects set recent $x
+        cd $x
+    fi
+}
